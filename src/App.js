d3981263338b42ddfad5eb925a4267ad48c73bd2
dgujs <jinsoo8457@dongguk.edu>
2020-03-04 16:48:51 +0900

    callAPI

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
 
-const moviesTitle = [
-  "a",
-  "b",
-  "c",
-  "d",
-];
 
-const movieImages = [
-  "temp",
-  "temp",
-  "temp",
-  "temp",
-];
+// Render : componentWillMount() -> render() -> componentDidMount()
+// 위 Cycle은 자동으로 발생한다.
 
-const App = () => {
-  return (
-    <div className="App">
-      <Movie title={moviesTitle[0]} poster= {movieImages[0]}/>
-      <Movie title={moviesTitle[1]} poster= {movieImages[1]}/>
-      <Movie title={moviesTitle[2]} poster= {movieImages[2]}/>
-      <Movie title={moviesTitle[3]} poster= {movieImages[3]}/>
-    </div>
-  );
+// Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> 
+
+class App extends Component {
+
+  state = {}
+
+  componentDidMount() {
+    this._getMoives()
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) =>
+      <Movie title={movie.title} poster={movie.large_cover_image} key={index} />
+    )
+    return movies
+  }
+
+  _getMoives = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+      .then(a => a.json())
+      .then(json => json.data.movies)
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.movies ? this._renderMovies() : 'Loading'}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,24 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Movie.css';
 
 
-class Movie extends Component {
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+const Movie = ({ title, poster }) => {
+    return (
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component {
-    render() {
-        console.log(this.props.poster);
-        return (
-            <img src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg" />
-        )
-    }
+const MoviePoster = ({ poster }) => {
+    return (
+        <img src={poster} />
+    )
 }
 export default Movie
